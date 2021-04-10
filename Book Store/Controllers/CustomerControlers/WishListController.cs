362b993c3b08c61a73e42a05d555e550d5c200f9
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-04-11 01:01:16 +0530

    REFACTORED

--- Book Store/Controllers/CustomerControlers/WishListController.cs
+++ Book Store/Controllers/CustomerControlers/WishListController.cs
@@ -79,8 +79,6 @@ namespace Book_Store.Controllers.CustomerControlers
                 {
                     IEnumerable<Claim> claims = identity.Claims;
                     string CustomerID = claims.Where(p => p.Type == "CustomerID").FirstOrDefault()?.Value;
-                    string UserType = claims.Where(p => p.Type == "UserType").FirstOrDefault()?.Value;
-
                     ICollection<CustomerWishList> WishList = customerWishListBL.RemoveBookFromWishList(CustomerID, BookID);
                     if (WishList != null)
                     {
